618353087885280bcaf99b51795ffb6ac2efd085
vishveshwar Kambli <65409872+vishveshkambli@users.noreply.github.com>
2023-10-14 16:59:09 +0530

    Add files via upload

---
+++ ProjectOne/projectone/appone/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for projectone project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .import views as v
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',v.homeF),
+    path('adduser1',v.adduser1f),
+    path('AddUser1',v.AddUser1F),
+    path('adduser2',v.adduser2f),
+    path('AddUser2',v.AddUser2F),
+    path('userlist',v.UserListF),
+]
