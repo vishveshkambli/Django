3ba86852ba93003e533ff1f52119b9608e44250c
vishveshwar Kambli <65409872+vishveshkambli@users.noreply.github.com>
2023-10-17 08:38:23 +0530

    Add files via upload

---
+++ OrmProjectOne/ormprojectone/appone/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Emp(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.CharField(max_length=30)
+    contact=models.CharField(max_length=30)
+    age=models.IntegerField()
+    address=models.TextField(max_length=300)
